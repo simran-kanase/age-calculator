72af8ae4f66933b3edb4e59f7843c4091765df39
simran-kanase <kanasesimran8@gmail.com>
2024-08-31 13:12:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+let userInput=document.getElementById("date");
+userInput.max=new Date().toISOString().split("T")[0];
+let btn=document.getElementById("button");
+let result = document.getElementById("result");
+
+btn.addEventListener("click",()=>{
+    let birthdate=new Date(userInput.value);
+
+    let d1=birthdate.getDate();
+    let m1=birthdate.getMonth()+1;
+    let y1=birthdate.getFullYear();
+
+    let today=new Date();
+    let d2=today.getDate();
+    let m2=today.getMonth()+1; //month start from 0
+    let y2=today.getFullYear();
+    let d3,m3,y3;
+    y3=y2-y1;
+    if(m2>=m1)
+        {
+            m3=m2-m1;
+        }
+    else
+    {
+        y3--;
+        m3=12+m2-m1;
+    }
+    if(d2>=d1)
+        {
+            d3=d2-d1;
+        }
+        else
+        {
+            m3--;
+            d3 = getDaysInMonth(y1,m1)+d2-d1;  
+        }
+    if(m3<0)
+        {
+            m3=11;
+            y3--;
+        }
+        console.log(d3,m3,y3)
+        result.innerHTML = `You are ${y3} years ,${m3}months, ${d3} days old.....!!!`;
+
+})
+
+function getDaysInMonth(year,month){
+    return new Date(year,month,0).getDate();
+}
+
+
+// function calculateAge(){
+//     let birthdate=new Date(userInput,value);
+
+//     let d1=birthdate.getDate();
+//     let m1=birthdate.getMonth()+1;
+//     let y1=birthdate.getFullYear();
+
+//     let today=new Date();
+//     let d2=today.getDate();
+//     let m2=today.getMonth()+1; //month start from 0
+//     let y2=today.getFullYear();
+//     let d3,m3,y3;
+//     y3=y2-y1;
+//     if(m2>=m1)
+//         {
+//             m3=m2-m1;
+//         }
+//     else
+//     {
+//         y3--;
+//         m3=12+m2-m1;
+//     }
+//     if(d2>=d1)
+//         {
+//             d3=d2-d1;
+//         }
+//         else
+//         {
+//             m3--;
+//             d3 = getDaysInMonth(y1,m1)+d2-d1;  
+//         }
+//     if(m3<0)
+//         {
+//             m3=11;
+//             y3--;
+//         }
+//         console.log(d3,m3,y3)
+// }
